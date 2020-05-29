623618960bbe58bd937ce0f3ea0c2434dc54a3cb
marax27 <kacpert314@gmail.com>
2020-05-29 22:46:31 +0200

    Add HtmlRawTextElement

---
+++ DokuWikiTranslator.Application/Generation/Features/HtmlRawTextElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using DokuWikiTranslator.Application.Generation.Abstractions;
+
+namespace DokuWikiTranslator.Application.Generation.Features
+{
+    public class HtmlRawTextElement : ISyntaxTreeNode
+    {
+        public string Text { get; }
+
+        public HtmlRawTextElement(string text)
+            => Text = text;
+
+        public string Generate()
+            => Text;
+
+        public IEnumerable<ISyntaxTreeNode> Children
+            => new List<ISyntaxTreeNode>();
+    }
+}
